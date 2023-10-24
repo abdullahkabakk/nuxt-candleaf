28b46ad45eeb5df5e89d41c415d324974cb87be4
Abdullah Kabak <05200000101@ogrenci.ege.edu.tr>
2023-10-25 01:39:59 +0300

    Added new libraries and config files
    - Added nuxt icon for icon library
    - Added nuxt/color-mode for dark mode
    - Added configurations for tailwind file

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxt/ui'],
-  devtools: { enabled: true }
+  modules: ['@nuxt/ui', 'nuxt-icon', '@nuxtjs/color-mode', '@nuxtjs/tailwindcss'],
+  colorMode: {
+    classSuffix: ''
+  },
+  tailwindcss: {
+    configPath: './tailwind.config.js'
+  },
+  devtools: {
+    enabled: true,
+
+    timeline: {
+      enabled: true
+    }
+  }
 })
---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+
+export default {
+    darkMode: 'class',
+    theme: {
+        extend: {
+            colors: {
+                background: {
+                    light: '#F3F3F3',
+                    dark: '#222831'
+                },
+                primary:  '#56B280',
+                card: {
+                    light: '#F7F8FA',
+                    dark: '#393E46'
+                },
+                cardImage: {
+                    light: '#DBE2EF',
+                    dark: '#252A34'
+                }
+            }
+        }
+    },
+    plugins: [],
+    content: []
+}

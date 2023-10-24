7ecb08cdedbcae640224a4545e427f13db28ff7f
Abdullah Kabak <05200000101@ogrenci.ege.edu.tr>
2023-10-25 01:41:09 +0300

    Added nature candle file for marketing

--- components/NatureCandle.vue
+++ components/NatureCandle.vue
@@ -2,9 +2,25 @@
 
 </script>
 
-<template>
-  $END$
-</template>
+  <template>
+    <div class="w-full relative max-h-screen -mt-12 transition-colors duration-300">
+      <img src="bg-image.png" alt="Background Image" class="max-h-screen w-screen">
+      <div class="w-full absolute  top-1/3 flex items-center justify-center">
+        <div class="w-full mx-4 lg:w-1/2 rounded-lg xl:w-1/3 transition-colors duration-300 flex flex-col p-16 text-black lg:mx-auto text-center bg-white/70 dark:bg-card-dark/50" id="marketing">
+          <h1 class="text-3xl">
+            <span class="block">&#127793;</span>
+            The Nature Candle
+          </h1>
+          <p class="text-center mt-2">
+            All handmade with natural soy wax, Candleaf is a companion for all your pleasure moments.
+          </p>
+          <button class="bg-primary dark:bg-card-dark w-1/2 mx-auto mt-10 py-2 text-white rounded-lg">
+            Discover Our Collection
+          </button>
+        </div>
+      </div>
+    </div>
+  </template>
 
 <style scoped>
 

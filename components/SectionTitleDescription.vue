ebef2db091b0c38a9872e5515c95866e4d4614f7
Abdullah Kabak <05200000101@ogrenci.ege.edu.tr>
2023-10-25 19:59:44 +0300

    Added section title description for home page

---
+++ components/SectionTitleDescription.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+const props = defineProps({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  }
+})
+</script>
+
+<template>
+  <h2 class="text-3xl text-center">{{ props.title }}</h2>
+  <p class="mt-2 text-center text-gray-600 dark:text-gray-300">{{ props.description }}</p>
+</template>

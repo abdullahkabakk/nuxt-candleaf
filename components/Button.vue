589b1c8e03cedc8cd4c6e7cd93b387ab335bd32c
Abdullah Kabak <05200000101@ogrenci.ege.edu.tr>
2023-10-26 00:48:50 +0300

    Added Button, Card, Column and Container components

---
+++ components/Button.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const props = defineProps({
+  name: {
+    type: String,
+    required: true,
+  },
+})
+</script>
+
+<template>
+  <button class="py-2 px-4 bg-primary hover:bg-transparent hover:border
+   hover:border-primary hover:text-primary text-white rounded-lg">
+    {{props.name}}
+  </button>
+</template>
---
+++ components/footer/Column.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import footerLinks from "~/utils/footerLinks";
+</script>
+
+<template>
+  <div v-for="column in footerLinks">
+    <h2 class="mb-6 text-sm font-semibold text-primary uppercase dark:text-primary">{{ column.title }}</h2>
+    <ul class="text-gray-500 dark:text-gray-400 font-medium">
+      <li class="mb-4" v-for="link in column.links">
+        <nuxt-link :to="link.link" class="hover:underline">{{ link.name }}</nuxt-link>
+      </li>
+    </ul>
+  </div>
+</template>

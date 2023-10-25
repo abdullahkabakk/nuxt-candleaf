a77ce3b21641aaa4327a489685f94f6b1966152e
Abdullah Kabak <05200000101@ogrenci.ege.edu.tr>
2023-10-26 00:53:35 +0300

    Nature Candle divided into MarketingCard

---
+++ components/MarketingCard.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="mx-4 lg:w-1/2 xl:w-1/3 bg-white/70 dark:bg-card-dark/50 text-black dark:text-white text-center rounded-lg flex flex-col p-16">
+    <h1 class="text-4xl">&#127793; <br> The nature candle</h1>
+    <p class="mt-2">All handmade with natural soy wax, Candleaf is a companion for all your pleasure moments.</p>
+    <Button class="md:w-1/2 mx-auto mt-10" name="Discover Our Collection"/>
+  </div>
+</template>
--- components/NatureCandle.vue
+++ components/NatureCandle.vue
@@ -1,26 +1,7 @@
-<script setup lang="ts">
-
-</script>
-
-  <template>
-    <div class="w-full h-screen bg-cover  transition-colors duration-300 bg-[url(bg-image.png)]">
-      <div class="w-full  my-auto h-full flex items-center justify-center">
-        <div class="w-full mx-4 lg:w-1/2 rounded-lg xl:w-1/3 transition-colors duration-300 flex flex-col p-16 text-black lg:mx-auto text-center bg-white/70 dark:bg-card-dark/50" id="marketing">
-          <h1 class="text-3xl">
-            <span class="block text-4xl">&#127793;</span>
-            The nature candle
-          </h1>
-          <p class="text-center mt-2">
-            All handmade with natural soy wax, Candleaf is a companion for all your pleasure moments.
-          </p>
-          <button class="bg-primary whitespace-nowrap w-full px-2 dark:bg-card-dark md:w-1/2 mx-auto mt-10 py-2 text-white rounded-lg">
-            Discover Our Collection
-          </button>
-        </div>
-      </div>
+<template>
+  <div class="h-screen bg-cover bg-[url(bg-image.png)] transition-colors duration-300">
+    <div class="my-auto h-full flex items-center justify-center">
+      <MarketingCard />
     </div>
+  </div>
   </template>
-
-<style scoped>
-
-</style>

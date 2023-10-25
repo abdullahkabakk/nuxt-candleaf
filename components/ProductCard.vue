3a527d14c17c51b943d83661973dec4a56d14953
Abdullah Kabak <05200000101@ogrenci.ege.edu.tr>
2023-10-25 20:02:08 +0300

    Exported products array

--- components/ProductCard.vue
+++ components/ProductCard.vue
@@ -1,75 +1,22 @@
 <script setup lang="ts">
-const products = [
-  {
-    img: 'image1.png',
-    name: 'Spiced Mint',
-    price: 9.99,
-    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.',
-    id: 1,
-  },
-  {
-    img: 'image1-1.png',
-    name: 'Sweet Strawberry',
-    price: 9.99,
-    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.',
-    id: 2,
-  },
-  {
-    img: 'image1-2.png',
-    name: 'Cool Blueberries',
-    price: 9.99,
-    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.',
-    id: 3,
-  },
-  {
-    img: 'image1-3.png',
-    name: 'Fresh Lemon',
-    price: 9.99,
-    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.',
-    id: 4,
-  },
-  {
-    img: 'image1-4.png',
-    name: 'Juicy Orange',
-    price: 9.99,
-    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.',
-    id: 5,
-  },
-  {
-    img: 'image1-5.png',
-    name: 'Sweet Cherry',
-    price: 9.99,
-    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.',
-    id: 6,
-  },
-  {
-    img: 'image1-6.png',
-    name: 'Fresh Apple',
-    price: 9.99,
-    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.',
-    id: 7,
-  },
-  {
-    img: 'image1-7.png',
-    name: 'Sweet Peach',
-    price: 9.99,
-    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.',
-    id: 8,
-  },
-]
+
+import products from "~/utils/products";
 </script>
 
 <template>
   <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 pb-10">
     <div v-for="product in products" :key="product.id">
-      <div class="shadow-lg hover:shadow-2xl rounded-lg bg-card-light dark:bg-card-dark transition-colors duration-300">
+      <div class="shadow-lg hover:shadow-2xl rounded-lg bg-card-light/20 dark:bg-card-dark transition-colors duration-300">
         <div class="bg-primary/10 dark:bg-cardImage-dark mx-auto transition-colors duration-300 rounded-t-lg">
           <img :src="product.img" alt="" class="mx-auto">
         </div>
-        <div class="mx-auto">
-          <h2 class="pt-2 hover:underline text-background-dark dark:text-background-light text-lg ml-2">{{ product.name }}</h2>
-          <p class="mt-3 text-center text-card-dark dark:text-card-light">{{ product.description }}</p>
-          <p class="mt-3 text-right mr-2 pb-2">{{ product.price }}$</p>
+        <div class="mx-auto p-4">
+          <h2 class="hover:underline font-semibold text-background-dark dark:text-background-light text-lg">{{ product.name }}</h2>
+          <p class="mt-3 text-card-dark dark:text-card-light">{{ product.description }}</p>
+          <div class="flex flex-row pb-2 mt-2">
+          <p class=" my-auto">{{ product.price }}$</p>
+          <button class="ml-auto py-1 bg-primary px-4 text-white rounded-lg">Add to cart</button>
+          </div>
         </div>
       </div>
     </div>

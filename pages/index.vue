b479d57127269e5bf42f854b4ed69f830e3f47f3
Abdullah Kabak <05200000101@ogrenci.ege.edu.tr>
2023-10-26 10:48:14 +0300

    Added seo tags for index page

--- pages/index.vue
+++ pages/index.vue
@@ -7,3 +7,12 @@
     <PopularProducts />
   </div>
 </template>
+
+<script setup lang="ts">
+useSeoMeta({
+  title: 'Candleaf - Handmade Soy Wax Candles',
+  ogTitle: 'Candleaf - Handmade Soy Wax Candles',
+  description: 'Discover handmade soy wax candles at Candleaf. Perfect for your moments of pleasure.',
+  ogDescription: 'Discover handmade soy wax candles at Candleaf. Perfect for your moments of pleasure.',
+})
+</script>

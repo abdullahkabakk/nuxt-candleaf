545170c6c4d85f148f3afabed3b07a3ba17a134a
Abdullah Kabak <05200000101@ogrenci.ege.edu.tr>
2023-10-26 10:45:33 +0300

    Removed seo tags in app.vue

--- app.vue
+++ app.vue
@@ -3,33 +3,3 @@
     <NuxtPage />
   </NuxtLayout>
 </template>
-
-<script setup>
-// Add meta tags for SEO
-onMounted(() => {
-  const head = document.head;
-  const title = document.createElement('title');
-  title.innerText = 'The Nature Candle - Handmade Soy Wax Candles';
-  head.appendChild(title);
-
-  const description = document.createElement('meta');
-  description.name = 'description';
-  description.content = 'Discover handmade soy wax candles at Candleaf. Perfect for your moments of pleasure.';
-  head.appendChild(description);
-
-  const ogTitle = document.createElement('meta');
-  ogTitle.property = 'og:title';
-  ogTitle.content = 'The Nature Candle - Handmade Soy Wax Candles';
-  head.appendChild(ogTitle);
-
-  const ogDescription = document.createElement('meta');
-  ogDescription.property = 'og:description';
-  ogDescription.content = 'Discover handmade soy wax candles at Candleaf. Perfect for your moments of pleasure.';
-  head.appendChild(ogDescription);
-
-  const ogImage = document.createElement('meta');
-  ogImage.property = 'og:image';
-  ogImage.content = 'https://example.com/your-image.jpg'; // Replace with your image URL
-  head.appendChild(ogImage);
-});
-</script>

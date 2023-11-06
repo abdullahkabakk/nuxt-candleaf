73feb285335036d1610c57fc8bc4369a936dc23c
Abdullah Kabak <05200000101@ogrenci.ege.edu.tr>
2023-11-07 00:35:09 +0300

    Added click event for button, added icon slot

--- components/Button.vue
+++ components/Button.vue
@@ -5,11 +5,18 @@ const props = defineProps({
     required: true,
   },
 })
+
+const emits = defineEmits(['button-click'])
+
+function onClick() {
+  emits('button-click')
+}
 </script>
 
 <template>
   <button class="py-2 px-4 bg-primary hover:bg-transparent hover:border
-   hover:border-primary hover:text-primary text-white rounded-lg">
+   hover:border-primary hover:text-primary text-white rounded-lg" @click="onClick">
+    <slot name="icon"/>
     {{props.name}}
   </button>
 </template>

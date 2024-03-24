deca2b06fb841ffe2f97edea82ff46d96ee53ae3
Abdullah Kabak <05200000101@ogrenci.ege.edu.tr>
2024-03-25 01:01:16 +0300

    [refactor] Added new menu item for mobile
    - Added new menu for mobile hamburger menu.
    - Refactored scripts, minimized scripts

--- components/navbar/HamburgerMenu.vue
+++ components/navbar/HamburgerMenu.vue
@@ -1,30 +1,17 @@
 <script setup lang="ts">
+import { items } from '~/utils/menu';
+
 const colorMode = useColorMode();
+const colorModeIcon = computed(() => (colorMode.value === 'light' ? 'i-heroicons-sun' : 'i-heroicons-moon'));
 
-const colorModeText = computed(() =>colorMode.value === "light" ? "Light Mode" : "Dark Mode")
-const colorModeIcon = computed(() => colorMode.value === "light" ? "i-heroicons-sun" : "i-heroicons-moon")
-
-function changeColorMode() {
-  colorMode.value = colorMode.value === "light" ? "dark" : "light"
-}
+const changeColorMode = () => (colorMode.value = colorMode.value === 'light' ? 'dark' : 'light');
 </script>
 
 <template>
-  <UPopover id="hamburger-button" aria-label="Hamburger Popover">
-    <UButton id="hamburger-menu" aria-label="Hamburger Menu" color="white" variant="link" trailing-icon="i-heroicons-bars-3-solid" />
-    <template #panel>
-      <div class="flex flex-col w-40 ml-auto space-y-2">
-        <NuxtLink to="/home">
-          <UButton label="About" size="lg" icon="i-heroicons-newspaper" color="primary" variant="ghost" class="dark:hover-bg-gray-800 pl-2 pr-0 gap-x-3 transition-colors w-full hover:bg-primary" />
-        </NuxtLink>
-        <NuxtLink to="/contact">
-          <UButton label="Contact" size="lg" icon="i-heroicons-phone" color="primary" variant="ghost" class="hover:bg-gray-100 pl-2 pr-0 gap-x-3 dark:hover-bg-gray-800 transition-colors" />
-        </NuxtLink>
-        <UButton label="Profile" size="lg" icon="i-heroicons-user" color="primary" variant="ghost" class="hover:bg-gray-100 pl-2 pr-0 gap-x-3 dark:hover-bg-gray-800 transition-colors" />
-        <UButton label="Cart" size="lg" icon="i-heroicons-shopping-cart" color="primary" variant="ghost" class="hover:bg-gray-100 pl-2 pr-0 gap-x-3 dark:hover-bg-gray-800 transition-colors" />
-        <hr class="mt-2 dark:bg-gray-600 dark:border-slate-700" />
-        <UButton @click="changeColorMode" :label="colorModeText" size="lg" :icon="colorModeIcon" color="primary" variant="ghost" class="transition-colors pl-2 pr-0 gap-x-3 text-black duration-300 w-full hover:bg-gray-100 whitespace-nowrap dark:hover-bg-gray-800" />
-      </div>
-    </template>
-  </UPopover>
+  <div class="flex">
+    <UButton @click="changeColorMode" size="lg" color="white" :icon="colorModeIcon" variant="ghost" />
+    <UDropdown :items="items" mode="hover" :popper="{ placement: 'bottom-start' }">
+      <UButton color="white" trailing-icon="i-heroicons-bars-3-solid" class="text-md font-normal" variant="ghost" />
+    </UDropdown>
+  </div>
 </template>
---
+++ utils/menu.ts
@@ -0,0 +1,44 @@
+export const items = [
+  [
+    {
+      label: 'Settings',
+      icon: 'i-heroicons-cog'
+    },
+    {
+      label: 'About',
+      icon: 'i-heroicons-newspaper',
+      to: '/home'
+    },
+    {
+      label: 'Contact',
+      icon: 'i-heroicons-phone',
+      to: '/contact'
+    },
+    {
+      label: 'Profile',
+      icon: 'i-heroicons-user'
+    },
+    {
+      label: 'Cart',
+      icon: 'i-heroicons-shopping-cart'
+    }
+  ],
+  [
+    {
+      label: 'Settings',
+      icon: 'i-heroicons-cog'
+    },
+    {
+      label: 'Logout',
+      icon: 'i-heroicons-arrow-right-end-on-rectangle'
+    }
+  ],
+  [
+    {
+      label: 'Profile',
+      avatar: {
+        src: 'https://avatars.githubusercontent.com/u/739984?v=4'
+      }
+    }
+  ]
+];

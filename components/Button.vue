0682957ba54145037e9f0f5148390e928dfd609d
Abdullah Kabak <05200000101@ogrenci.ege.edu.tr>
2024-03-25 01:02:03 +0300

    [refactor] Added type for define props
    - Added type for ProductCard, Button, CustomerReviewCard

--- components/Button.vue
+++ components/Button.vue
@@ -1,22 +1,17 @@
 <script setup lang="ts">
-const props = defineProps({
-  name: {
-    type: String,
-    required: true,
-  },
-})
+defineProps(['name']);
 
-const emits = defineEmits(['button-click'])
-
-function onClick() {
-  emits('button-click')
-}
+const emits = defineEmits(['button-click']);
+const onClick = () => emits('button-click');
 </script>
 
 <template>
-  <button class="py-2 px-4 bg-primary hover:bg-transparent hover:border
-   hover:border-primary hover:text-primary text-white rounded-lg" @click="onClick">
-    <slot name="icon"/>
-    {{props.name}}
+  <button
+    name="button"
+    class="py-2 px-4 bg-primary hover:bg-transparent hover:border hover:border-primary hover:text-primary text-white rounded-lg"
+    @click="onClick"
+  >
+    <slot name="icon" />
+    {{ name }}
   </button>
 </template>
--- components/footer/SocialLink.vue
+++ components/footer/SocialLink.vue
@@ -1,23 +1,11 @@
 <script setup lang="ts">
-const props = defineProps({
-  icon: {
-    type: String,
-    required: true,
-  },
-  link: {
-    type: String,
-    required: true,
-  },
-  sr: {
-    type: String,
-    required: true,
-  },
-})
+import type { SocialLink } from '~/types/socialLink';
+defineProps<SocialLink>();
 </script>
 
 <template>
-  <nuxt-link :to="props.link" class="text-gray-500 hover:text-gray-900 dark:hover:text-white">
-    <Icon :name="props.icon ?? '' " class="h-5 w-5" />
-    <span class="sr-only">{{ props.sr }}</span>
+  <nuxt-link :to="link" class="text-gray-500 hover:text-gray-900 dark:hover:text-white">
+    <Icon :name="icon" class="h-5 w-5" />
+    <span class="sr-only">{{ sr }}</span>
   </nuxt-link>
 </template>

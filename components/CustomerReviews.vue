33c45b0065f27b656288711f854c975e5ed6c818
Abdullah Kabak <05200000101@ogrenci.ege.edu.tr>
2024-03-25 00:57:45 +0300

    [refactor] Added localizations strings

--- components/CustomerReviews.vue
+++ components/CustomerReviews.vue
@@ -1,17 +1,20 @@
 <script setup lang="ts">
-import SectionTitleDescription from "~/components/SectionTitleDescription.vue";
-import customers from "~/utils/customers";
+import SectionTitleDescription from '~/components/SectionTitleDescription.vue';
+import customers from '~/utils/customers';
 </script>
 
 <template>
   <div class="bg-primary/10 dark:bg-background-dark py-20">
-    <SectionTitleDescription title="Testimonials" description="Some quotes from our happy customers" />
-      <Container>
-        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
-          <div v-for="customer in customers">
-            <CustomerReviewCard :customer="customer" />
-          </div>
+    <SectionTitleDescription
+      :title="$t('home.customerReviews.title')"
+      :description="$t('home.customerReviews.description')"
+    />
+    <Container>
+      <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
+        <div v-for="customer in customers">
+          <CustomerReviewCard :customer="customer" />
         </div>
-      </Container>
+      </div>
+    </Container>
   </div>
 </template>
--- components/MarketingCard.vue
+++ components/MarketingCard.vue
@@ -1,7 +1,12 @@
 <template>
-  <div class="mx-4 lg:w-1/2 xl:w-1/3 bg-white/70 dark:bg-card-dark/50 text-black dark:text-white text-center rounded-lg flex flex-col p-16">
-    <h1 class="text-4xl">&#127793; <br> The nature candle</h1>
-    <p class="mt-2">All handmade with natural soy wax, Candleaf is a companion for all your pleasure moments.</p>
-    <Button class="md:w-2/3 mx-auto mt-10 whitespace-nowrap" name="Discover Our Collection"/>
+  <div
+    class="mx-4 lg:w-1/2 xl:w-1/3 bg-white/70 dark:bg-card-dark/50 text-black dark:text-white text-center rounded-lg flex flex-col p-16"
+  >
+    <h1 class="text-4xl">
+      &#127793; <br />
+      {{ $t('home.marketingCard.title') }}
+    </h1>
+    <p class="mt-2">{{ $t('home.marketingCard.description') }}</p>
+    <Button class="md:w-2/3 mx-auto mt-10 whitespace-nowrap" :name="$t('home.marketingCard.button')" />
   </div>
 </template>

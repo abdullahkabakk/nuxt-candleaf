31e5667a8422804287b94f8d0ba7e9066451fb0a
Abdullah Kabak <05200000101@ogrenci.ege.edu.tr>
2024-03-25 00:50:20 +0300

    [feat] Implemented i18n config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,18 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxt/ui', 'nuxt-icon', "@nuxt/image", '@formkit/auto-animate/nuxt'],
+  modules: ['@nuxt/ui', 'nuxt-icon', '@nuxt/image', '@formkit/auto-animate/nuxt', '@nuxtjs/i18n'],
   colorMode: {
     classSuffix: ''
+  },
+  i18n: {
+    vueI18n: './i18n.config.ts'
   },
   tailwindcss: {
     configPath: './tailwind.config.js'
   },
-  app:{
-  head: {
-    htmlAttrs: {
-      lang: 'en-US'
+  app: {
+    head: {
+      htmlAttrs: {
+        lang: 'en-US'
+      }
     }
-  }
   },
   devtools: {
     enabled: true,
@@ -21,4 +24,4 @@ export default defineNuxtConfig({
       enabled: true
     }
   }
-})
+});

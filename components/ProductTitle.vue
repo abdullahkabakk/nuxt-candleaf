c77a47b2360905234f51c3d831efe48666d28a34
Abdullah Kabak <05200000101@ogrenci.ege.edu.tr>
2024-03-25 03:29:57 +0300

    [feat] Added product title and description

---
+++ components/ProductTitle.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+defineProps<{ name: string; description: string }>();
+</script>
+
+<template>
+  <h1 class="text-2xl font-semibold">{{name}}</h1>
+  <p>{{description}}</p>
+</template>

f58c4174784b258090fd463dc25bcfb0048d1474
Abdullah Kabak <05200000101@ogrenci.ege.edu.tr>
2024-03-25 21:49:26 +0300

    [feat] Implemented cart store logic
    - Our cart state logic includes adding, updating, removing item in cart

---
+++ stores/cart.ts
@@ -0,0 +1,43 @@
+import type CartInfo from '~/types/cartInfo';
+
+interface State {
+  cart: CartInfo[];
+}
+
+export const useCartStore = defineStore('cart', {
+  state: (): State => {
+    const cart: CartInfo[] = [];
+    return {
+      cart: cart
+    };
+  },
+  persist: true,
+  getters: {
+    getCart: (state) => state.cart,
+    getCartLength: (state) => state.cart.length
+  },
+  actions: {
+    addToCart(item: CartInfo): void {
+      const existingItemIndex = this.cart.findIndex((cartItem) => cartItem.id === item.id);
+      if (existingItemIndex !== -1) {
+        // If item already exists in cart, update its quantity
+        this.cart[existingItemIndex] = item;
+      } else {
+        // Otherwise, push the new item to cart
+        this.cart.push(item);
+      }
+    },
+    updateCart(id: number, quantity: number): void {
+      const itemIndex = this.cart.findIndex((cartItem) => cartItem.id === id);
+      if (itemIndex !== -1) {
+        this.cart[itemIndex].count = quantity;
+      }
+    },
+    removeFromCart(id: number): void {
+      const itemIndex = this.cart.findIndex((cartItem) => cartItem.id === id);
+      if (itemIndex !== -1) {
+        this.cart.splice(itemIndex, 1);
+      }
+    }
+  }
+});

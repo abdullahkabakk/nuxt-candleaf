c11f7de7e79501f1a0423a3198b3116448aa77eb
Abdullah Kabak <05200000101@ogrenci.ege.edu.tr>
2024-03-27 02:04:05 +0300

    [feat] Added payment layout

---
+++ layouts/payment.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="flex flex-col max-w-screen min-h-screen transition-colors duration-500 bg-white dark:bg-background-dark">
+    <div class="mx-4 flex justify-between max-w-screen-xl my-3 w-full xl:mx-auto">
+      <NuxtLink to="/" aria-label="Candleaf Logo">
+        <NuxtImg src="/logo.png" alt="Candleaf Logo" format="webp" width="126" height="34" />
+      </NuxtLink>
+    </div>
+    <div>
+      <CartDetailsBreadCrumb/>
+      <NuxtPage />
+    </div>
+  </div>
+</template>

e9c2a27f35efedb4c05e16833d4beedc3476386b
Abdullah Kabak <05200000101@ogrenci.ege.edu.tr>
2024-03-27 02:02:53 +0300

    [feat] Added input field

---
+++ components/form/TheInput.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+defineProps<{
+  placeholder: string;
+  icon?: string;
+}>();
+</script>
+
+<template>
+  <div class="relative">
+    <input
+      type="text"
+      :placeholder="placeholder"
+      class="w-full border border-gray-300 rounded-md py-3 px-4 pr-10 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+    />
+    <div v-if="icon" class="absolute inset-y-0 right-0 pr-3 flex items-center pointer-events-none">
+      <Icon :name="icon" class="h-5 w-5 text-gray-400" />
+    </div>
+  </div>
+</template>

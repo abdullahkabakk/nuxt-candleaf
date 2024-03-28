bec0dd22d1706c446f9b734a7a902f8c1d18b4f0
Abdullah Kabak <05200000101@ogrenci.ege.edu.tr>
2024-03-29 00:06:34 +0300

    [feat] Added new fields to discovery menu
    - Removed all initial values in discovery dropdown
    - Added Most Searched, New Season and Most Sold fields

--- utils/menu.ts
+++ utils/menu.ts
@@ -1,44 +1,17 @@
 export const items = [
   [
     {
-      label: 'Settings',
-      icon: 'i-heroicons-cog'
+      label: 'New Season',
+      icon: 'i-heroicons-newspaper'
     },
     {
-      label: 'About',
-      icon: 'i-heroicons-newspaper',
-      to: '/home'
+      label: 'Most Searched',
+      icon: 'i-heroicons-magnifying-glass'
     },
     {
-      label: 'Contact',
-      icon: 'i-heroicons-phone',
-      to: '/contact'
+      label: 'Most Sold',
+      icon: 'i-heroicons-shopping-bag'
     },
-    {
-      label: 'Profile',
-      icon: 'i-heroicons-user'
-    },
-    {
-      label: 'Cart',
-      icon: 'i-heroicons-shopping-cart'
-    }
+
   ],
-  [
-    {
-      label: 'Settings',
-      icon: 'i-heroicons-cog'
-    },
-    {
-      label: 'Logout',
-      icon: 'i-heroicons-arrow-right-end-on-rectangle'
-    }
-  ],
-  [
-    {
-      label: 'Profile',
-      avatar: {
-        src: 'https://avatars.githubusercontent.com/u/739984?v=4'
-      }
-    }
-  ]
 ];

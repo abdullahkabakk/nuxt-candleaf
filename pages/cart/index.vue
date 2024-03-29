52bf882fc7042806f57e1e7454fe8475193a4f16
Abdullah Kabak <05200000101@ogrenci.ege.edu.tr>
2024-03-30 00:51:44 +0300

    [feat] Added localization for cart page

--- pages/cart/index.vue
+++ pages/cart/index.vue
@@ -70,22 +70,22 @@
       </div>
       <!-- Payment Summary -->
       <div class="col-span-1">
-        <h2 class="font-bold text-lg">Payment Summary</h2>
+        <h2 class="font-bold text-lg">{{ $t('cart.paymentSummary') }}</h2>
         <div
           class="sticky top-20 dark:border-gray-600 right-0 mt-5 border text-gray-700 dark:text-gray-300 rounded-lg p-4 shadow-md"
         >
           <!-- Payment Details -->
           <div class="flex justify-between mb-3">
-            <span class="">Subtotal:</span>
+            <span>{{ $t('cart.subtotal') }}:</span>
             <span class="font-semibold">${{ calculateSubtotal().toFixed(2) }}</span>
           </div>
           <div class="flex justify-between mb-3">
-            <span class="">Shipping:</span>
-            <span>Free</span>
+            <span>{{ $t('cart.shipping') }}:</span>
+            <span>{{ $t('cart.free') }}</span>
           </div>
           <hr class="my-2 border-gray-200 dark:border-gray-600" />
           <div class="flex justify-between font-semibold text-lg">
-            <span>Total:</span>
+            <span>{{ $t('cart.total') }}:</span>
             <span>${{ calculateTotal().toFixed(2) }}</span>
           </div>
           <!-- Checkout Button -->
@@ -93,7 +93,7 @@
             @click="$router.push('/cart/details')"
             class="mt-4 w-full py-2 bg-primary text-white rounded-md focus:outline-none hover:bg-primary-dark transition duration-300 ease-in-out transform hover:scale-105"
           >
-            Proceed to Checkout
+            {{ $t('cart.proceedToCheckout') }}
           </button>
         </div>
       </div>

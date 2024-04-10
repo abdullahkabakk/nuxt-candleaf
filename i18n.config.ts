5cc8f1e72d0a0eb21636c5b1574933b484e05721
Abdullah Kabak <05200000101@ogrenci.ege.edu.tr>
2024-04-11 00:33:45 +0300

    [feat] Exported i18n config to use as global

--- i18n.config.ts
+++ i18n.config.ts
@@ -1,9 +1,16 @@
 import en from './locales/en.json';
+import { createI18n } from 'vue-i18n';
 
-export default defineI18nConfig(() => ({
+const i18nConfig = {
   legacy: false,
+  globalInjection: true,
   locale: 'en',
+  fallbackLocale: 'en',
   messages: {
     en: en
   }
-}));
+};
+
+export default defineI18nConfig(() => i18nConfig);
+
+export const i18n = createI18n(i18nConfig);

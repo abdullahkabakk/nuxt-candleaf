0c758ca8c7ef42ee42363e0ce6f14ba3bd3968cd
Abdullah Kabak <05200000101@ogrenci.ege.edu.tr>
2024-04-11 00:31:54 +0300

    [feat] Added routing for profile for navbar
    - Instead of doing nothing, our profile button redirects to login page
    - Using storeToRefs to get any getter/function from store since that we are getting getCartLength function from store

--- components/navbar/MidSection.vue
+++ components/navbar/MidSection.vue
@@ -1,14 +1,5 @@
 <template>
-  <UDropdown :items="items" mode="hover" :popper="{ placement: 'bottom-start' }">
-    <UButton
-      color="white"
-      :label="$t('home.nav.discovery')"
-      trailing-icon="i-heroicons-chevron-down-20-solid"
-      class="text-md font-normal"
-      variant="ghost"
-    />
-  </UDropdown>
-  <div class="my-auto space-x-4">
+  <div class="my-auto mt-1 space-x-4">
     <NuxtLink v-for="link in ['', 'contact', 'about']" :to="`/${link}`" class="hover:underline">{{
       link === '' ? $t('home.nav.home') : $t(`home.nav.${link}`)
     }}</NuxtLink>

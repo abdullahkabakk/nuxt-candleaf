1df303dbddfebdf30d9362b5cb57d901981206c9
Abdullah Kabak <05200000101@ogrenci.ege.edu.tr>
2024-04-11 00:35:50 +0300

    [feat] Implemented register page

---
+++ pages/register.vue
@@ -0,0 +1,36 @@
+<template>
+  <Container>
+    <div class="max-w-2xl mx-auto">
+      <h1 class="text-3xl font-semibold text-center mb-2">{{ $t('register.title') }}</h1>
+      <p class="text-center text-gray-600 dark:text-gray-200 mb-6">{{ $t('register.description') }}</p>
+      <VForm @submit="handleSubmit" :validation-schema="registerSchema">
+        <Builder :form-fields="registerFields" button-text="Register" class="w-full">
+          <template #beforeButton>
+            <div class="mt-4 flex ml-auto text-sm md:text-base justify-end flex-row">
+              <p class="text-gray-600 dark:text-gray-200">{{ $t('register.alreadyHaveAccount') }}</p>
+              <nuxt-link to="/login" class="text-primary hover:underline ml-1">{{
+                $t('register.clickHereToLogin')
+              }}</nuxt-link>
+            </div>
+          </template>
+        </Builder>
+      </VForm>
+    </div>
+  </Container>
+</template>
+
+<script setup lang="ts">
+import { registerFields, registerSchema } from '~/forms/register';
+
+useSeoMeta({
+  title: 'Register - Candleaf',
+  ogTitle: 'Register - Candleaf',
+  description: 'Register to Candleaf to get access to exclusive deals and discounts.',
+  ogDescription: 'Register to Candleaf to get access to exclusive deals and discounts.'
+});
+
+const handleSubmit = (val: any, resetForm: any) => {
+  console.log(val);
+  // Handle form submission here
+};
+</script>
